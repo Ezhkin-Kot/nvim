2a12beba4bc1cf2618921dded868cbc3ce24119d
Ezhkin-Kot <ezkot.tokze@gmail.com>
2025-05-06 12:30:18 +0400

    feat: add mapping for dap

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,8 +1,16 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
+
+-- DAP
+map("n", "<leader>du", function() require("dapui").open() end, { desc = "DAP UI Toggle" })
+map("n", "<F5>", function() require("dap").continue() end, { desc = "DAP: Continue" })
+map("n", "<F10>", function() require("dap").step_over() end, { desc = "DAP: Step Over" })
+map("n", "<F11>", function() require("dap").step_into() end, { desc = "DAP: Step Into" })
+map("n", "<S-F11>", function() require("dap").step_out() end, { desc = "DAP: Step Out" })
+map("n", "<S-F5>", function() require("dap").terminate() end, { desc = "DAP: Terminate" })
+map("n", "<F9>", function() require("dap").toggle_breakpoint() end, { desc = "DAP: Toggle Breakpoint" })
+map("n", "<leader>dr", function() require("dap").restart() end, { desc = "DAP: Restart" })
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
