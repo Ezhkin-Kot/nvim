d0c0c9c17333c0d69209ab974bc4a7c798bb7397
Ezhkin-Kot <ezkot.tokze@gmail.com>
2025-05-07 09:12:19 +0400

    feat: make clangd work with non-ascii

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,7 +1,5 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require("lspconfig")
-
 local servers = {
   "html",
   "cssls",
@@ -12,8 +10,22 @@ local servers = {
 
 vim.lsp.enable(servers)
 
+local base = require "nvchad.configs.lspconfig"
+local on_attach = base.on_attach
+local capabilities = base.capabilities
+
+local lspconfig = require("lspconfig")
+
 lspconfig.omnisharp.setup({
   cmd = { "omnisharp" },
   filetypes = { "cs", "vb" },
   root_dir = lspconfig.util.root_pattern("*.sln", "*.csproj", ".git"),
 })
+
+lspconfig.clangd.setup {
+  on_attach = function(client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufnr)
+  end,
+  capabilities = capabilities,
+}
