1d3345a04a7553ca9c90dabcab9a55996d2184d0
Ezhkin-Kot <ezkot.tokze@gmail.com>
2025-05-08 14:59:31 +0400

    chore: add .gitignore

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -7,6 +7,38 @@ local options = {
     -- html = { "prettier" },
   },
 
+  formatters = {
+    -- C & C++
+    ["clang-format"] = {
+      prepend_args = {
+        "-style={ \
+                    IndentWidth: 4, \
+                    TabWidth: 4, \
+                    UseTab: Never, \
+                    AccessModifierOffset: 0, \
+                    IndentAccessModifiers: true, \
+                    PackConstructorInitializers: Never}",
+      },
+    },
+    -- Golang
+    ["goimports-reviser"] = {
+        prepend_args = { "-rm-unused" },
+    },
+    golines = {
+        prepend_args = { "--max-len=80" },
+    },
+    -- Lua
+    stylua = {
+        prepend_args = {
+            "--column-width", "80",
+            "--line-endings", "Unix",
+            "--indent-type", "Spaces",
+            "--indent-width", "4",
+            "--quote-style", "AutoPreferDouble",
+        },
+    },
+  },
+
   -- format_on_save = {
   --   -- These options will be passed to conform.format()
   --   timeout_ms = 500,
@@ -14,4 +46,4 @@ local options = {
   -- },
 }
 
-return options
+require("conform").setup(options)
