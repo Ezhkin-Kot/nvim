fc470160c7bf5cc5f5b2a9a7dab7a83d6f1b9887
Ezhkin-Kot <ezkot.tokze@gmail.com>
2025-06-01 17:42:39 +0400

    fix: lazy windsurf config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -66,6 +66,14 @@ return {
 
   {
     "Exafunction/windsurf.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+    },
+    config = function()
+        require("codeium").setup({
+        })
+    end,
     event = "BufEnter",
   },
 
