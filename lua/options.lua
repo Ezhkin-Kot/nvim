3ead15d74d9c09c2b47a759a208f2ee9b62d9484
Ezhkin-Kot <ezkot.tokze@gmail.com>
2025-09-13 18:50:07 +0400

    feat: 4-spaces tab for C/C++

--- lua/options.lua
+++ lua/options.lua
@@ -11,3 +11,13 @@ o.tabstop = 2
 o.expandtab = true
 o.smartindent = true
 o.shiftwidth = 2
+
+-- tab settings for C/C++
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "cpp" },
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+  end,
+})
