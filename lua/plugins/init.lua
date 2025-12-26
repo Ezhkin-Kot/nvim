37f3da9be5cc708cf84c589c630a03e6a416905c
Ezhkin-Kot <ezkot.tokze@gmail.com>
2025-12-27 00:40:13 +0400

    feat: add auto switch to english input in normal mode

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -173,6 +173,25 @@ return {
     end,
   },
 
+  { "nvim-lua/plenary.nvim", lazy = true },
+
+  {
+    "drop-stones/im-switch.nvim",
+    event = "VeryLazy",
+    opts = {
+      macos = {
+        enabled = true, -- Set to true if you are on macOS
+        default_im = "com.apple.keylayout.ABC", -- or your preferred input method
+      },
+      linux = {
+        enabled = false, -- Set to true if you are on Linux
+        default_im = "keyboard-us", -- or your preferred input method
+        get_im_command = { "fcitx5-remote", "-n" }, -- { "ibus", "engine" }
+        set_im_command = { "fcitx5-remote", "-s" }, -- { "ibus", "engine" }
+      },
+    },
+  },
+
   -- Code completion
   {
     "Exafunction/windsurf.nvim",
